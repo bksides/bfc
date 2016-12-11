d447e15ee76937313622b49af11a2108fd3c6508
Brandon <bksides.@utexas.edu>
2016-12-10 20:02:16 -0700

    Added help menu; modified readme; began implementing io optimization

--- bfc.c
+++ bfc.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <sys/wait.h>
 
+int buf_size = 128;
+int 
 
 struct looptrack
 {
@@ -14,7 +16,47 @@ struct looptrack
 
 void help()
 {
-	printf("No help menu yet.  Stay tuned :^)\n");
+	printf(
+	"\nbfc: a brainfuck compiler for 64 bit linux systems\n\n"
+
+	"bfc <input file> [-o <output file>] [-v] [-h] [-c <compilation level=3>]\n\n"
+
+	"PARAMETERS:\n\n"
+
+	"\tinput file: This parameter is a path to the input file.\n\n"
+
+	"OPTIONS:\n\n"
+
+	"\t-o\tThis option allows the user to specify a path to an output file\n"
+	"\t\tto which the final output shall be written.  The file need not\n"
+	"\t\texist prior to the invocation of bfc.\n\n"
+
+	"\t\tParameters:\n\n"
+
+	"\t\toutput file: The path of the output file to be written.\n\n"
+
+	"\t-v\tDirects bfc to print its version information prior to\n"
+	"\t\tinitiating any compilation.\n\n"
+
+	"\t-h\tDirects bfc to print usage help prior to initiating any\n"
+	"\t\tcompilation.\n\n"
+
+	"\t-c\tAllows the user to specify how far down the compilation chain\n"
+	"\t\tbfc shall go.\n\n"
+
+	"\t\tParameters:\n\n"
+
+	"\t\tcompilation level: The point at which to stop compilation\n\n"
+
+	"\t\t1 indicates to stop after producing assembly output.\n\n"
+
+	"\t\t2 indicates to stop after producing an object file.\n\n"
+
+	"\t\t3 indicates to compile down to executable form.\n\n"
+
+	"\t\tIf the -c option is not added, a compilation level of\n"
+	"\t\t3 will be used.\n"
+	);
 }
 
 void version()
@@ -59,17 +101,64 @@ void print_err()
 
 int compile(FILE* in, FILE* out)
 {
-	fprintf(out, ".data\n"
+	fprintf(out, ".bss\n"
+				 "OUTBUF:\n"
+				 ".skip %d\n"
+				 "DESTBUF:\n"
+				 ".skip %d\n"
+				 "INBUF:\n"
+				 ".skip %d\n\n"
+				 "STACK:\n"
+				 ".skip %d\n\n"
+
+				 ".data\n"
 				 "ARR:\n"
-				 ".space 32768\n"
+				 ".space 32768\n\n"
+
 				 ".text\n"
+				 ".globl _flush_input\n"
+				 ".type _flush_input, @function\n"
+
+				 "_flush_input:"
+				 "\tcmpq $DESTBUF, %%rsp\n"
+				 "\tjne DO_INPUT_FLUSH\n"
+				 "\tretq\n"
+				 "\tDO_INPUT_FLUSH:\n"
+				 "\tcmpq $OUTBUF, %%rbp\n"
+				 "\tje NO_OUT_FLUSH:\n"
+				 "\tcallq _flush_output\n"
+				 "\tNO_OUT_FLUSH:\n"
+				 "\tmovq $0, %%rax\n"
+				 "\tmovq $0, %%rdi\n"
+				 "\tmovq $INBUF, %%rsi\n"
+				 "\tmovq %%rsp, %%rdx\n"
+				 "\tsubq $DESTBUF, %%rdx\n"
+				 "\tshr $4, %%rdx\n"
+				 "\tsyscall\n"
+				 "\tmovq $DESTBUF, %%r9\n"
+				 "\tmovq $INBUF, %%r10\n"
+				 "\tCOPY:\n"
+				 "\tmovq (%%r9), %%r11\n"
+				 "\tmovb (%%r10), %%bl\n"
+				 "\tmovb %%bl, (%%r11)\n"
+				 "\tcmpq %%r9, %%rsp\n"
+				 "\tjne NEXT\n"
+				 "\tretq\n"
+				 "\tNEXT:\n"
+				 "\taddq 4, %%r9\n"
+				 "\tincq %%r10\n"
+				 "\tjmp COPY\n"
 				 ".globl _start\n"
 				 ".type _start, @function\n"
+
 				 "_start:\n"
-				 "\tmovq $ARR, %%r8\n");
+				 "\tmovq $ARR, %%r8\n"
+				 "\tmovq $OUTBUF, %%rbp\n"
+				 "\tmovq $DESTBUF, %%rsp\n");
 	int pincs = 0;
 	int vincs = 0;
 	int label = 0;
+	int input_needs_flush = 0;
 	struct looptrack* last = NULL;
 	char c = fgetc(in);
 	while(c != EOF)
@@ -81,6 +170,10 @@ int compile(FILE* in, FILE* out)
 		}
 		else if(c != '+' && c != '-' && vincs)
 		{
+			if(input_needs_flush)
+			{
+				fprintf(out, "\tcallq _flush_input\n");
+			}
 			fprintf(out, "\t%s $%d, (%%r8)\n", vincs > 0 ? "addb" : "subb", abs(vincs));
 			vincs = 0;
 		}
@@ -214,10 +307,12 @@ int assemble(char* in, char* out, int complev)
 							"\theaders PT_PHDR PHDRS ;\n"
 							"\ttext PT_LOAD FILEHDR PHDRS ;\n"
 							"\tdata PT_LOAD ;\n"
+							"\tbss PT_LOAD ;\n"
 							"}\n"
 							"SECTIONS\n"
 							"{\n"
   							"\t. = 0x400000;\n"
+  							"\t.bss . : { *(.bss) } :bss\n"
   							"\t.text . : { *(.text) } :text\n"
   							"\t. = ALIGN(4096);\n"
   							"\t.data . : { *(.data) } :data\n"
@@ -306,7 +401,7 @@ int main(int argc, char** argv)
 			}
 		}
 	}
-	else if(!purposeful || output != NULL)
+	else if(!purposeful && output == NULL)
 	{
 		fprintf(stderr, "Fatal Error: No input file specified.\n");
 		return 1;
